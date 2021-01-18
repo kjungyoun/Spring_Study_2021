8e44f885f748803a0e462c068193223817fef737
kjungyoun <rlawjddbs963@naver.com>
2021-01-18 09:22:57 +0900

    ~2021.01.15 Start Camp

---
+++ WordCloud2/src/main/java/com/ssafy/word/WordCloud2Application.java
@@ -0,0 +1,13 @@
+package com.ssafy.word;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordCloud2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordCloud2Application.class, args);
+	}
+
+}
